bb61e41eb1baf15ba720dc40d53826091e90b2c5
lizhuoxiong <lizx244@annto.com.cn>
2022-07-21 14:05:57 +0800

    feat:controller入口嵌套校验List

--- valid_start/src/main/java/com/xkt/valid/controller/UserController.java
+++ valid_start/src/main/java/com/xkt/valid/controller/UserController.java
@@ -8,16 +8,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.List;
+
 /**
  * @author lzx
  * @date 2022/7/21.
  */
+@Validated
 @RequestMapping("/user")
 @RestController
 public class UserController {
 
     /**
      * 基本校验
+     *
      * @param user
      * @return
      */
@@ -28,11 +33,25 @@ public class UserController {
 
     /**
      * 嵌套校验
+     *
      * @param user
      * @return
      */
     @PostMapping("/nest")
     public String nestSave(@Validated @RequestBody UserNest user) {
+        return "OK";
+    }
+
+    /**
+     * 嵌套List校验
+     *
+     * 注意 若这里嵌套在controller入口处为List则需要在整个controller上加@Validated才生效
+     *
+     * @param user
+     * @return
+     */
+    @PostMapping("/nestList")
+    public String nestListSave(@RequestBody List<@Valid User> user) {
         return "OK";
     }
 }
