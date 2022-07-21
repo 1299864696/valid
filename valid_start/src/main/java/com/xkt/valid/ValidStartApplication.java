bcef4cc4402acf7311b94c7b9cc544f9af76ce27
lizhuoxiong <lizx244@annto.com.cn>
2022-07-21 11:14:22 +0800

    init

---
+++ valid_start/src/main/java/com/xkt/valid/ValidStartApplication.java
@@ -0,0 +1,13 @@
+package com.xkt.valid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidStartApplication.class, args);
+    }
+
+}
---
+++ valid_start/src/test/java/com/xkt/valid/ValidStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xkt.valid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
