dc6528ad9c1406503c185388b6ef8c1e62a4cc20
lizhuoxiong <lizx244@annto.com.cn>
2022-07-21 14:13:51 +0800

    feat:controller入口嵌套校验List-借助@InitBinder进行校验

--- valid_start/src/main/java/com/xkt/valid/controller/UserController.java
+++ valid_start/src/main/java/com/xkt/valid/controller/UserController.java
@@ -2,7 +2,11 @@ package com.xkt.valid.controller;
 
 import com.xkt.valid.dto.User;
 import com.xkt.valid.dto.UserNest;
+import com.xkt.valid.validator.ListValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +19,11 @@ import java.util.List;
  * @author lzx
  * @date 2022/7/21.
  */
-@Validated
 @RequestMapping("/user")
 @RestController
 public class UserController {
-
+    @Autowired
+    private ListValidator validator;
     /**
      * 基本校验
      *
@@ -53,5 +57,25 @@ public class UserController {
     @PostMapping("/nestList")
     public String nestListSave(@RequestBody List<@Valid User> user) {
         return "OK";
+    }
+
+    /**
+     * 嵌套List校验
+     *
+     * 注意 若这里嵌套在controller入口处为List则需要在整个controller上加@Validated才生效
+     *
+     * @param user
+     * @return
+     */
+    @PostMapping("/nestList2")
+    public String nestListSave2(@RequestBody @Validated List<User> user) {
+        return "OK";
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        if(binder.getTarget() instanceof List) {
+            binder.addValidators(validator);
+        }
     }
 }
---
+++ valid_start/src/main/java/com/xkt/valid/validator/ListValidator.java
@@ -0,0 +1,29 @@
+package com.xkt.valid.validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.List;
+
+@Component
+public class ListValidator implements Validator {
+    @Autowired
+    private LocalValidatorFactoryBean validator;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return List.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        List<?> list = (List<?>)target;
+        for (Object o: list) {
+            validator.validate(o, errors);
+        }
+    }
+
+}
